531d60f01ec1acbce622689a09f37c58d8508360
scottGitHub2016 <scott.business@gmail.com>
2023-11-15 11:00:15 +0000

    Pending changes exported from your codespace

--- examples/properties_for_sale.py
+++ examples/properties_for_sale.py
@@ -1,11 +1,14 @@
-from daftlistings import Daft, Location, SearchType, PropertyType
+from daftlistings import Daft, Location, SearchType, PropertyType, Distance
 
 daft = Daft()
-daft.set_location(Location.DUBLIN)
+
+#Distance = Distance.KM0;
+daft.set_location([Location.SLIGO, Location.ROSCOMMON], Distance.KM20)#probably wouldnt needd the distance thing just cover all counties and then use google maps
 daft.set_search_type(SearchType.RESIDENTIAL_SALE)
 daft.set_property_type(PropertyType.HOUSE)
-daft.set_min_price(400000)
-daft.set_max_price(500000)
+daft.set_min_price(150000)
+daft.set_max_price(320000)
+
 
 listings = daft.search()
 
@@ -13,4 +16,18 @@ for listing in listings:
     print(listing.title)
     print(listing.price)
     print(listing.daft_link)
+    print("Agent Name:",listing.agent_name)
+    print("Agency:",listing.agent_branch)
+    print("Longatude:", listing.latitude)
+    print("Latitude:", listing.longitude)   
+    print(listing.size_meters_squared, "m²")
+    print(listing.bathrooms)
+    print( listing.bedrooms)
+    print("**TODO**")
+    print("Mortgage Monthly Est")
+    print("Mortgage Total Interest")
+    print("Distance to Crozon")
+    print("Fibre BB")
+    print("5G Signal")
+    #print(listing.distance_to(54.2612883,-8.4743678))
     print()
